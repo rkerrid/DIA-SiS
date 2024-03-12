e9c3e5ef5875f58bc8386204c8e0c94307be3919
rkerrid <robbiejamesk@protonmail.com>
2024-03-12 16:46:06 +0100

    Changed href functions to use precursor level

--- dia_sis/pipeline/generate_protein_groups.py
+++ dia_sis/pipeline/generate_protein_groups.py
@@ -26,12 +26,15 @@ class HrefRollUp:
         quantification = 'Precursor.Translated'
         # formatting and ratios
         self.formatted_precursors = self.format_silac_channels(self.filtered_report)
-        self.formatted_precursors = self.calculate_precursor_ratios(self.formatted_precursors, quantification)
-        self.protein_groups = self.compute_protein_level_test(self.formatted_precursors)
+        self.formatted_precursors = self.calculate_precursor_ratios(self.formatted_precursors)
+        self.href = self.calculate_precursor_href_intensities(self.formatted_precursors)
+        self.protein_groups = self.compute_protein_level(self.formatted_precursors)
         # self.protein_groups = self.compute_protein_level_test(self.formatted_precursors)
 
         # Adjusting intensities and outputing data
-        self.protein_groups = self.calculate_href_intensities(self.protein_groups)
+        # self.protein_groups = self.calculate_href_intensities(self.protein_groups)
+        self.protein_groups = self.calculate_href_intensities_test(self.protein_groups, self.href) #uses precursor median
+
         self.output_protein_groups(self.protein_groups, self.path)
         end_time = time.time()
         print(f"Time taken to generate protein groups: {end_time - start_time} seconds")
@@ -56,39 +59,14 @@ class HrefRollUp:
  
         return merged_df
     
-    def calculate_precursor_ratios(self, df, quantification):
-        print(f'Calculating SILAC ratios based on Ms1.Translated and Precursor.Translated')
+    def calculate_precursor_ratios(self, df):
+        print('Calculating SILAC ratios based on Ms1.Translated and Precursor.Translated')
         # Calculate ratios for all chanels (Precursor.Quantity is the total intensity of all 3 chanels, the default diann value has been overwritten at this point)
         df['Precursor.Translated H/T'] = df['Precursor.Translated H'] / (df['Precursor.Translated L'] + df['Precursor.Translated H'])
         df['Ms1.Translated H/T'] = df['Ms1.Translated H'] / (df['Ms1.Translated L'] + df['Ms1.Translated H'])
         return df
-    
-    def compute_protein_level(self, df): # this function looks for at least 3 valid values for each ratio and sums Precursor.Quantity (which is the sum of precursor translated values) for total intensity
-        print('Rolling up to protein level')
-        # Function to filter values and compute median
-        def valid_median(series):
-            valid_series = series.replace([0, np.inf, -np.inf], np.nan).dropna()
-            valid_series = np.log2(valid_series)
-            return 2 **valid_series.median() if len(valid_series) >= 1 else np.nan
-        
-        def valid_sum(series):
-            valid_series = series.replace([0, np.inf, -np.inf], np.nan).dropna()
-            return valid_series.sum() 
-        
-        result = df.groupby(['Protein.Group', 'Run']).agg({
-            'Precursor.Translated H/T': valid_median,
-            'Precursor.Translated L/T': valid_median,
-            'Precursor.Quantity': valid_sum        
-        })
-        result['H'] = result['Precursor.Translated H/T']*result['Precursor.Quantity']
-        result['L'] = result['Precursor.Translated L/T']*result['Precursor.Quantity']
-        result = result.reset_index()
-        
-        cols = ['Run', 'Protein.Group', 'Precursor.Quantity', 'H', 'L'] 
-        return result[cols]
-    
 
-    def compute_protein_level_test(self, df):
+    def compute_protein_level(self, df):
         print('Rolling up to protein level')
         runs = df['Run'].unique()
         runs_list = []
@@ -109,14 +87,14 @@ class HrefRollUp:
                 else:
                     return np.nan
     
-            def valid_sum(series):
+            def valid_median(series):
                 valid_series = series.replace([0, np.inf, -np.inf], np.nan).dropna()
-                return valid_series.sum()
+                return valid_series.median()
     
             # Group by protein group and apply the custom aggregation
             grouped = run_df.groupby(['Protein.Group']).apply(lambda x: pd.Series({
                 'H/T ratio': combined_median(x['Ms1.Translated H/T'], x['Precursor.Translated H/T']),
-                'Precursor.Quantity': valid_sum(x['Precursor.Quantity'])
+                'Precursor.Quantity': valid_median(x['Precursor.Quantity'])                
             })).reset_index()
             
             grouped['Run'] = run
@@ -125,34 +103,72 @@ class HrefRollUp:
         result = pd.concat(runs_list, ignore_index=True)
         result['H'] = result['H/T ratio']*result['Precursor.Quantity']
         result['L'] = result['Precursor.Quantity'] - result['H']
-        cols = ['Run','Protein.Group', 'H', 'L', 'H/T ratio', 'Precursor.Quantity']
+        cols = ['Run','Protein.Group', 'H', 'L', 'H/T ratio',  'Precursor.Quantity']
     
         # Returning the dataframe with specified columns
         return result[cols]
 
 
-    # Adjust unnormalized intensities
-    def calculate_href_intensities(self, df):
+    # # Adjust unnormalized intensities
+    # def calculate_href_intensities(self, df):
+    #     print('Calculating adjusted intensities using reference')
+    #     df_copy = df.copy(deep=True)
+        
+    #     # Calculate median H value and reset index to make it a DataFrame
+    #     h_ref = df_copy.groupby('Protein.Group')['H'].median().reset_index()
+        
+    #     # Rename the median column to 'h_ref'
+    #     h_ref = h_ref.rename(columns={'H': 'h_ref'})
+        
+    #     # Merge the original DataFrame with the h_ref DataFrame
+    #     merged_df = df.merge(h_ref, on='Protein.Group', how='inner')
+        
+    #     # calculate factor to multiply other chanels by dividing href by original H intensity for each PG
+    #     merged_df['factor'] = merged_df['h_ref']/merged_df['H']
+        
+    #     # Normalize other chanels with this factor
+    #     merged_df['H_norm'] = merged_df['H']*merged_df['factor']
+    #     merged_df['L_norm'] = merged_df['L']*merged_df['factor']
+        
+    #     return merged_df
+    
+    def calculate_precursor_href_intensities(self, df):
+        
+        def combined_median(ms1_series, precursor_series):
+            # Replace invalid values with NaN and drop them
+            valid_ms1 = ms1_series.replace([0, np.inf, -np.inf], np.nan).dropna()
+            valid_precursor = precursor_series.replace([0, np.inf, -np.inf], np.nan).dropna()
+
+            # Ensure at least 3 valid values in each series before combining
+            if len(valid_ms1) >= 1 and len(valid_precursor) >= 1:
+                combined_series = np.concatenate([valid_ms1, valid_precursor])
+                combined_series = np.log2(combined_series)  # Log-transform the combined series
+                return 2 ** np.median(combined_series)  # Return the median of the log-transformed values
+            else:
+                return np.nan
+
+        # Group by protein group and apply the custom aggregation
+        grouped = df.groupby(['Protein.Group']).apply(lambda x: pd.Series({
+            'href': combined_median(x['Ms1.Translated H'], x['Precursor.Translated H'])
+        })).reset_index()
+
+        return grouped[['Protein.Group', 'href']]
+    
+    def calculate_href_intensities_test(self, protein_groups, href):
         print('Calculating adjusted intensities using reference')
-        df_copy = df.copy(deep=True)
+     
         
-        # Calculate median H value and reset index to make it a DataFrame
-        h_ref = df_copy.groupby('Protein.Group')['H'].median().reset_index()
-        
-        # Rename the median column to 'h_ref'
-        h_ref = h_ref.rename(columns={'H': 'h_ref'})
-        
-        # Merge the original DataFrame with the h_ref DataFrame
-        merged_df = df.merge(h_ref, on='Protein.Group', how='inner')
+        merged_df = protein_groups.merge(href, on='Protein.Group', how='inner')
         
         # calculate factor to multiply other chanels by dividing href by original H intensity for each PG
-        merged_df['factor'] = merged_df['h_ref']/merged_df['H']
+        merged_df['factor'] = merged_df['href']/merged_df['H']
         
         # Normalize other chanels with this factor
         merged_df['H_norm'] = merged_df['H']*merged_df['factor']
         merged_df['L_norm'] = merged_df['L']*merged_df['factor']
         
         return merged_df
+    
     
     def output_protein_groups(self, df, path):
         manage_directories.create_directory(self.path, 'protein_groups')
